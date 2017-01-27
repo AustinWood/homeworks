25a0c22bc44a00fef9fddd01035cf82866bf9199
Austin Wood <austin@austinkwood.com>
2017-01-26 23:33:46 -0800

    Solve problem 3

--- W1D5/abstract_data_types.rb
+++ W1D5/abstract_data_types.rb
@@ -37,3 +37,36 @@ class Queue
     @queue.dup
   end
 end
+
+# Exercise 3 - Map
+class Map
+  def initialize
+    @map = []
+  end
+
+  def assign(key, value)
+    found_key = false
+    @map.each do |el|
+      if el[0] == key
+        el[1] = value
+        found_key = true
+      end
+    end
+    @map << [key, value] unless el_index
+  end
+
+  def lookup(key)
+    @map.each { |el| return el[1] if el[0] == key }
+    nil
+  end
+
+  def remove(key)
+    @map.each_with_index do |el, i|
+      @map.delete_at(i) if el[0] == key
+    end
+  end
+
+  def show
+    @map.dup
+  end
+end
