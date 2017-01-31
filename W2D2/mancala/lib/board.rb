d6db454587b94aaaccfd740aa2fc0ca482d57fdb
Austin Wood <austinwood@Austins-MacBook-Pro.local>
2017-01-31 09:05:21 -0800

    Distrubute stones from one cup around board

--- W2D2/mancala/lib/board.rb
+++ W2D2/mancala/lib/board.rb
@@ -2,6 +2,8 @@ class Board
   attr_accessor :cups
 
   def initialize(name1, name2)
+    @cups = Array.new(14) { [:stone] * 4 }
+    @cups[6], @cups[13] = [], []
   end
 
   def place_stones
@@ -9,10 +11,29 @@ class Board
   end
 
   def valid_move?(start_pos)
+    if start_pos >= @cups.length || start_pos <= 1
+      raise ArgumentError.new "Invalid starting cup"
+    end
   end
 
   def make_move(start_pos, current_player_name)
+  stones = @cups[start_pos]
+  @cups[start_pos] = []
+  cup_idx = start_pos
+  until stones.empty?
+    cup_idx += 1
+    cup_idx = 0 if cup_idx > 13
+    if cup_idx == 6
+      @cups[6] << stones.pop if current_player_name == @name1
+    elsif cup_idx == 13
+      @cups[13] << stones.pop if current_player_name == @name2
+    else
+      @cups[cup_idx] << stones.pop
+    end
   end
+  render
+  next_turn(cup_idx)
+end
 
   def next_turn(ending_cup_idx)
     # helper method to determine what #make_move returns
