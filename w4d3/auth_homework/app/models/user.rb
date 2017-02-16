eae8b988d6103555e6a38d592733360d4f93df9b
Austin Wood <austin@austinkwood.com>
2017-02-16 09:09:28 -0800

    Add w4d1 hw and w4d3 project

--- w4d3/auth_homework/app/models/user.rb
+++ w4d3/auth_homework/app/models/user.rb
@@ -22,18 +22,27 @@ class User < ActiveRecord::Base
 
   def find_by_credentials(username, password)
     user = User.find_by(username: username)
+    return user if user && BCrypt::Password.new(user.password_digest).is_password?(password)
+    nil
   end
 
   def generate_session_token
+    SecureRandom::urlsafe_base64(16)
   end
 
   def reset_session_token!
+    self.session_token = User.generate_session_token
+    self.save!
+    self.session_token
   end
 
   def ensure_session_token
+    self.session_token ||= User.generate_session_token
   end
 
-  def password=
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
   end
 
   private
